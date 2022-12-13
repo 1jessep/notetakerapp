e85e1947d8f5b16162a6a2915d7d7055744c7c2c
1jessep <jessepstynchcomb@gmail.com>
2022-12-13 14:53:02 -0600

    finished git and post route

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const util = require("util");
+const readFromFile = util.promisify(fs.readFile);
+const writeToFile = util.promisify(fs.writeFile);
+const getNotes = () => {
+  return readFromFile("db/db.json", "utf-8").then((rawNotes) =>
+    [].concat(JSON.parse(rawNotes))
+  );
+};
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  getNotes().then((notes) => res.json(notes));
+});
+app.post("/api/notes", (req, res) => {
+  getNotes().then((oldNotes) => {
+    var newArray = [
+      ...oldNotes,
+      { title: req.body.title, text: req.body.text, id: uuidv4() },
+    ];
+    writeToFile("db/db.json", JSON.stringify(newArray)).then(() =>
+      res.json({ msg: "okay" })
+    );
+  });
+});
+app.delete("/api/notes/:id", (req, res) => {});
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
